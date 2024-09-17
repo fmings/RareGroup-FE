08d057012b1dea0276c5819e61cb331e3c971a88
fmings <mingsfelicia@gmail.com>
2024-09-16 19:04:04 -0500

    added Post Card Component to my posts

--- API/PostData.js
+++ API/PostData.js
@@ -27,8 +27,21 @@ const getSinglePost = (postId) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+// GET ALL POSTS BY LOGGED IN USER
+const getAuthUserPosts = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/user/${id}/posts`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
 export {
   getAllPosts,
   getSinglePost,
-
+  getAuthUserPosts,
 };
