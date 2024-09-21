5f205cd3c0af6cbc037d552d0968b67d982655e6
fmings <mingsfelicia@gmail.com>
2024-09-21 10:30:46 -0500

    arranged category, styled tags on details

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import { getSinglePost } from '../../API/PostData';
 
 export default function ViewPost() {
@@ -23,22 +24,32 @@ export default function ViewPost() {
       <div>
         <h1>{postDetails.title || ''}</h1>
       </div>
+      <div className="details-header">
+        <div className="category-details">
+          {postDetails.categories ? (
+            <p className="post-category">Category: {postDetails.categories.label}</p>
+          ) : (
+            <p>No category selected</p>
+          )}
+        </div>
+      </div>
       <div className="d-flex flex-column">
         <img src={postDetails.imageUrl} alt={postDetails.title} className="post-details-img" />
       </div>
       <div className="text-dark details">
         <p>Posted on {formattedDate || ''}</p>
         <p className="post-content">{postDetails.content || ''}</p>
-        {postDetails.categories ? (
-          <p className="post-category">Category: {postDetails.categories.label}</p>
-        ) : (
-          <p>No category selected</p>
-        )}
-        {postDetails.tags ? postDetails.tags.map((tag) => (
-          <p key={tag.id} className="tag">
-            {tag.label}
-          </p>
-        )) : ''}
+        <div className="tagContainer">
+          {postDetails.tags ? postDetails.tags.map((tag) => (
+            <Button
+              key={tag.id}
+              variant="outline-secondary"
+              className="tag-btn-details"
+            >
+              {tag.label}
+            </Button>
+          )) : ''}
+        </div>
         <hr />
       </div>
     </div>
